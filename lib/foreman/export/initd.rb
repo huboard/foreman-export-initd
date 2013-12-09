8432995d0ef4a96c2be3827d5b15bc6ef44a3379
Tomasz Durka <tomasz@durka.pl>
2013-12-09 16:09:40 +0100

    Remove init scripts on startup

--- lib/foreman/export/initd.rb
+++ lib/foreman/export/initd.rb
@@ -12,8 +12,13 @@ class Foreman::Export::Initd < Foreman::Export::Base
       script = Pathname.new(cwd).join(args.shift)
       concurrency = engine.formation[name]
 
-      initscript = Initd::Script.new path, script, args, concurrency, user
-      initscript.export
+      Dir.glob(export_to.join("#{app}-*")).each do |filename|
+        File.unlink filename
+      end
+      if concurrency > 0
+        initscript = Initd::Script.new path, script, args, concurrency, user
+        initscript.export
+      end
     end
   end
 end
