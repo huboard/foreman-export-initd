56eb3fa3ee9ab489f21dfdccc373c48067311e76
Tomasz Durka <tomasz@durka.pl>
2013-12-13 02:38:09 +0100

    Customize user and application for initd tests

--- spec/foreman/export/initd_spec.rb
+++ spec/foreman/export/initd_spec.rb
@@ -11,13 +11,13 @@ describe Foreman::Export::Initd, :fakefs do
       file.path
     end
   end
-  let(:options) { Hash.new }
+  let(:options) { {:app => 'custom-app', :user => 'dummy'} }
   let(:engine) { Foreman::Engine.new().load_procfile(procfile) }
   let(:initd) { Foreman::Export::Initd.new('/tmp/initd', engine, options) }
 
   it 'exports to the filesystem' do
     initd.export
-    File.read('/tmp/initd/app-foo').should == spec_resource('initd/app-foo')
-    File.read('/tmp/initd/app-bar').should == spec_resource('initd/app-bar')
+    File.read('/tmp/initd/custom-app-foo').should == spec_resource('initd/custom-app-foo')
+    File.read('/tmp/initd/custom-app-bar').should == spec_resource('initd/custom-app-bar')
   end
 end
