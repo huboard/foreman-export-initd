5c3a766c8c57d75014b7b9426f0362984c5827a2
tomaszdurka <tomasz@durka.pl>
2014-02-28 12:23:15 +0100

    Use pathnames in exported list, export to /etc/init.d

--- spec/foreman/export/initd_spec.rb
+++ spec/foreman/export/initd_spec.rb
@@ -1,9 +1,16 @@
-require 'spec_helper'
 require 'foreman-export-initd'
+require 'spec_helper'
 require 'foreman/engine'
 require 'tmpdir'
 
 describe Foreman::Export::Initd, :fakefs do
+
+  class Foreman::Export::Initd
+    def exec_command command
+      # Ignore command
+    end
+  end
+
   let(:procfile_initial) do
     FileUtils.mkdir_p('/tmp/app')
     File.open('/tmp/app/Procfile-initial', 'w') do |file|
@@ -22,15 +29,15 @@ describe Foreman::Export::Initd, :fakefs do
 
   it 'exports to the filesystem' do
     engine = Foreman::Engine.new().load_procfile(procfile_initial)
-    initd = Foreman::Export::Initd.new('/tmp/initd', engine, options)
+    initd = Foreman::Export::Initd.new('/etc/init.d', engine, options)
     initd.export
-    File.read('/tmp/initd/custom-app-foo').should == spec_resource('initd/custom-app-foo')
-    File.read('/tmp/initd/custom-app-bar').should == spec_resource('initd/custom-app-bar')
+    File.read('/etc/init.d/custom-app-foo').should == spec_resource('initd/custom-app-foo')
+    File.read('/etc/init.d/custom-app-bar').should == spec_resource('initd/custom-app-bar')
 
     engine = Foreman::Engine.new().load_procfile(procfile_changed)
-    initd = Foreman::Export::Initd.new('/tmp/initd', engine, options)
+    initd = Foreman::Export::Initd.new('/etc/init.d', engine, options)
     initd.export
-    File.exists?('/tmp/initd/custom-app-foo').should == false
-    File.read('/tmp/initd/custom-app-bar').should == spec_resource('initd/custom-app-bar')
+    File.exists?('/etc/init.d/custom-app-foo').should == false
+    File.read('/etc/init.d/custom-app-bar').should == spec_resource('initd/custom-app-bar')
   end
 end
